cd5e3da2a37559071de2f667759d9bb78f1ba727
Julien Cholin <jcholin@eliocity.com>
2018-04-26 11:55:34 +0200

    fix(trip): add stats object with distance and duration

--- sdk-api/src/main/kotlin/com/xee/sdk/api/model/Trip.kt
+++ sdk-api/src/main/kotlin/com/xee/sdk/api/model/Trip.kt
@@ -27,8 +27,7 @@ import java.util.*
  * @since 4.0.0
  */
 data class Trip @JvmOverloads constructor(@SerializedName("id") var id: String,
-                                          @SerializedName("mileage") var mileage: Mileage? = null,
-                                          @SerializedName("usedTime") var usedTime: UsedTime? = null,
+                                          @SerializedName("stats") var stats: Stats? = null,
                                           @SerializedName("startLocation") var startLocation: Location? = null,
                                           @SerializedName("endLocation") var endLocation: Location? = null,
                                           @SerializedName("vehicleId") var vehicleId: String? = null,
@@ -37,8 +36,7 @@ data class Trip @JvmOverloads constructor(@SerializedName("id") var id: String,
 
     constructor(source: Parcel) : this(
             source.readString(),
-            source.readParcelable<Mileage>(Mileage::class.java.classLoader),
-            source.readParcelable<UsedTime>(UsedTime::class.java.classLoader),
+            source.readParcelable<Stats>(Stats::class.java.classLoader),
             source.readParcelable<Location>(Location::class.java.classLoader),
             source.readParcelable<Location>(Location::class.java.classLoader),
             source.readString(),
@@ -50,8 +48,7 @@ data class Trip @JvmOverloads constructor(@SerializedName("id") var id: String,
 
     override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
         writeString(id)
-        writeParcelable(mileage, 0)
-        writeParcelable(usedTime, 0)
+        writeParcelable(stats, 0)
         writeParcelable(startLocation, 0)
         writeParcelable(endLocation, 0)
         writeString(vehicleId)
@@ -64,70 +61,6 @@ data class Trip @JvmOverloads constructor(@SerializedName("id") var id: String,
         val CREATOR: Parcelable.Creator<Trip> = object : Parcelable.Creator<Trip> {
             override fun createFromParcel(source: Parcel): Trip = Trip(source)
             override fun newArray(size: Int): Array<Trip?> = arrayOfNulls(size)
-        }
-    }
-}
-
-/**
- * Mileage of the [Trip]
- * @author Julien Cholin
- * @since 4.0.0
- */
-data class Mileage @JvmOverloads constructor(@SerializedName("type") var type: String? = null,
-                                             @SerializedName("value") var value: Double = 0.0,
-                                             @SerializedName("unit") var unit: String? = null) : Parcelable {
-
-    constructor(source: Parcel) : this(
-            source.readString(),
-            source.readDouble(),
-            source.readString()
-    )
-
-    override fun describeContents() = 0
-
-    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
-        writeString(type)
-        writeDouble(value)
-        writeString(unit)
-    }
-
-    companion object {
-        @JvmField
-        val CREATOR: Parcelable.Creator<Mileage> = object : Parcelable.Creator<Mileage> {
-            override fun createFromParcel(source: Parcel): Mileage = Mileage(source)
-            override fun newArray(size: Int): Array<Mileage?> = arrayOfNulls(size)
-        }
-    }
-}
-
-/**
- * UsedTime of the [Trip]
- * @author Julien Cholin
- * @since 4.0.0
- */
-data class UsedTime @JvmOverloads constructor(@SerializedName("type") var type: String? = null,
-                                              @SerializedName("value") var value: Double = 0.0,
-                                              @SerializedName("unit") var unit: String? = null) : Parcelable {
-
-    constructor(source: Parcel) : this(
-            source.readString(),
-            source.readDouble(),
-            source.readString()
-    )
-
-    override fun describeContents() = 0
-
-    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
-        writeString(type)
-        writeDouble(value)
-        writeString(unit)
-    }
-
-    companion object {
-        @JvmField
-        val CREATOR: Parcelable.Creator<UsedTime> = object : Parcelable.Creator<UsedTime> {
-            override fun createFromParcel(source: Parcel): UsedTime = UsedTime(source)
-            override fun newArray(size: Int): Array<UsedTime?> = arrayOfNulls(size)
         }
     }
 }
